cc8284f148f84b9b17178beaa6d815a4e1cc37c4
Engku Nazri <engkunazri0609@gmail.com>
2021-05-16 18:58:11 +0800

    working on login

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,21 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticationController extends Controller
 {
     public function showSignInForm()
     {
+        if (!Auth::guest()){
+            return redirect('/');
+        }
+
         return view('Home.login');
     }
 
-    public function signIn()
+    public function signIn(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $request->session()->regenerate();
+
+            return redirect('/');
+        }
+
+        return back()->with('error', 'The provided credential do not match our records');
     }
 
     public function signOut()
     {
-        //
+        Auth::logout();
+
+        return redirect('logout', 'You have been successfully sign out from the website ;D');
     }
 }
--- resources/views/Home/login.blade.php
+++ resources/views/Home/login.blade.php
@@ -20,6 +20,17 @@
     <div class="form mx-auto">
         <h2 class="text-danger fw-bold text-center pt-4">Sign In</h2>
         <p class="fst-italic text-center pb-4">Wake up it f00d's o clock</p>
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+        @if (session('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @endif
+
         <form action="" class="p-2">
             @csrf
             <div class="mb-3">
@@ -32,7 +43,7 @@
             </div>
 
             <div class="d-grid gap-2 pt-5">
-                <button class="btn btn-danger" type="button">Sign In</button>
+                <button class="btn btn-danger">Sign In</button>
                 <a href="/register" class="btn btn-outline-danger">Sign Up</a>
                 <small><a href="/forget-password" class="text-decoration-none">Forget your password?</a></small>
             </div>
